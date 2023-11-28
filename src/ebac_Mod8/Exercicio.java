c9906f7c447af9bf895c862560f06fed8050fe56
beralta <pedrohugoferreira@gmail.com>
2023-11-28 17:43:51 -0300

    exercice mod8

---
+++ src/ebac_Mod8/Exercicio.java
@@ -0,0 +1,20 @@
+package ebac_Mod8;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+
+        int nota = 10;
+        int nota2 = 5;
+        int nota3 = 20;
+        int nota4 = 2;
+
+        double somaValor = nota + nota2 + nota3 + nota4;
+        double media = somaValor / 4;
+
+        System.out.println(media);
+
+    }
+
+
+}
