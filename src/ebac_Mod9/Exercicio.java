e92b28f771563f236da1210a5de35ef83fd4a7ac
beralta <pedrohugoferreira@gmail.com>
2023-11-29 01:09:03 -0300

    exercice Mod9

---
+++ src/ebac_Mod9/Exercicio.java
@@ -0,0 +1,11 @@
+package ebac_Mod9;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        int numero = 1;
+        System.out.println(numero);
+        numero = Integer.valueOf(1000000000);
+        System.out.println(numero);
+
+    }
+}
