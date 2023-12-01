c315fda15a9a6319f7f187ab2d606ba86ef5c48d
beralta <pedrohugoferreira@gmail.com>
2023-12-01 18:50:02 -0300

    correção de exercicio

--- src/ebac_Mod10/Exercicio.java
+++ src/ebac_Mod10/Exercicio.java
@@ -6,16 +6,20 @@ public class Exercicio {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
+        int somaNota = 0;
         for(int i = 1; i <= 4; i++) {
             System.out.println("Digite a " + i + "° nota: ");
             int lerNota = ler.nextInt();
-            if (lerNota >= 7) {
-                System.out.println("Aprovado!");
-            } else if(lerNota >= 5) {
-                System.out.println("Recuperação!");
-            } else {
-                System.out.println("Reprovado!");
-            }
+            somaNota += lerNota;
+        }
+
+        int mediaNota = somaNota / 4;
+        if (mediaNota >= 7) {
+            System.out.println("Aprovado!");
+        } else if(mediaNota >= 5) {
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado!");
         }
 
 
