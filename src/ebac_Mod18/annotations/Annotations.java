d355976cda3df004a7ee5046a86de6f4aa2aeec4
beralta <pedrohugoferreira@gmail.com>
2024-09-27 23:24:31 -0300

    annotations in pratic

--- src/ebac_Mod18/annotations/Annotations.java
+++ src/ebac_Mod18/annotations/Annotations.java
@@ -1,4 +1,8 @@
 package ebac_Mod18.annotations;
 
+@PrimeiraAnnotation(value = "Pedrin", bairros = {"laranjal", "Mutondo"})
 public class Annotations {
+
+    @PrimeiraAnnotation(value = "Xubaca", bairros = {"Culubande", "tribobó"})
+    private String name;
 }
---
+++ src/ebac_Mod18/annotations/PrimeiraAnnotation.java
@@ -0,0 +1,11 @@
+package ebac_Mod18.annotations;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+public @interface PrimeiraAnnotation {
+    String value();
+    String[] bairros();
+}
